f0432fea831707e8856aae7771b28cc4d1e6396e
Harrison Woodward <harrisonwoodward@Harrisons-MacBook-Pro.local>
2014-07-02 12:43:36 -0400

    added post to step form

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140702134825) do
+ActiveRecord::Schema.define(version: 20140702163944) do
 
   create_table "steps", force: true do |t|
     t.string   "title"
     t.string   "tags"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "post"
   end
 
   create_table "users", force: true do |t|
