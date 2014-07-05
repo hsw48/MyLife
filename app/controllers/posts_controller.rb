e12ed196b984c9c633854ca8e7d434d3213ed2ff
Gardner Fiveash <jg5ash4@gmail.com>
2014-07-05 15:55:37 -0400

    pseudo code for updating posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,21 @@
 class PostsController < ApplicationController
 
 	def new
-
 		@post = Post.new 
-
 	end 
 
+	def create
+		# find the step that we are interested (Step.find(...))
+
+		# create a post with the proper body
+
+		# add that post to the posts of the step we're interested in 
+
+		# redirect to the step that we just added a post to
+
+		raise
+		# redirect_to "/steps/#{params[:]}"
+	end
 
 
 	
