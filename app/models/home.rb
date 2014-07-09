790566423397d6201d4449f3bd092a243eaf5231
harrisonwoodward48 <harrisonwoodward48@gmail.com>
2014-07-09 09:58:18 -0400

    delete icon and picture box

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,9 +1,4 @@
 class Home < ActiveRecord::Base
- 	
-	def profile_pic
-    	if self.provider == "facebook" && self.uid
-      "https://graph.facebook.com/#{self.uid}/picture"
-    	end
-	end
+ 
 
 end
